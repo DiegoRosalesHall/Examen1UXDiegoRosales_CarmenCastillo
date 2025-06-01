b9d1e2be33f7faf5482f3ef40d58d9dad204a959
Carmen Castillo <castillocandrea@gmail.com>
2025-06-01 14:48:47 -0600

    pues ya esta el carrusel creo

--- src/components/molecules/descripcion.tsx
+++ src/components/molecules/descripcion.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import TextoAtomico from "../atoms/texto";
 import styles from "./descripcion.module.css";
 
-const Descripcion: React.FC<{ src: string; descripcion: string }> = ({ src, descripcion }) => {
+const Descripcion: React.FC<{ src: string; descripcion: string; textbtn: string;}> = ({ src, descripcion, textbtn}) => {
   return (
-    <div className={`carousel-caption d-none d-md-block ${styles.contenedortitulo}`}>
+    <div className={`carousel-caption d-md-block ${styles.contenedortitulo}`}>
       <img src={src} className={`${styles.tituloimagen}`} alt="Título en imagen" />
       <TextoAtomico contenido={descripcion} />
+      <button type="button" className={`btn ${styles.boton}`}> {textbtn} </button>
     </div>
   );
 };
--- src/components/organisms/carrusel.tsx
+++ src/components/organisms/carrusel.tsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import Poster from "../atoms/posterCar";
 import Boton from "../atoms/button";
 import BotonProgreso from "../atoms/botonProgreso";
-import Descripcion from "../molecules/descripcion"
+import Descripcion from "../molecules/descripcion";
+import styles from './Carrusel.module.css';
 
 const Carrusel = () => {
     const [indice, setIndice] = useState(0);
-    const peliculas = [{ src: "../img/carrusel1.webp", titulo: "../img/cartitulo1.webp", descripcion: "Descripción 1" }, { src: "../img/carrusel2.webp", titulo: "../img/cartitulo2.webp", descripcion: "Descripción 2" }, { src: "../img/carrusel3.webp", titulo: "../img/cartitulo3.webp", descripcion: "Descripción 3" }, { src: "../img/carrusel4.webp", titulo: "../img/cartitulo4.webp", descripcion: "Descripción 4" }, { src: "../img/carrusel5.webp", titulo: "../img/cartitulo5.webp", descripcion: "Descripción 5" }, { src: "../img/carrusel6.webp", titulo: "../img/cartitulo6.webp", descripcion: "Descripción 6" } ];
+    const peliculas = [{ src: "../img/carrusel1.jpg", titulo: "../img/cartitulo1.webp", descripcion: "\n16+   4 temporadas \nHacks explora la relación entre Deborah Vance, una legendaria \ncomediante de Las Vegas, y una joven desconocida con mucho que decir.", textbtn:"Ir a la serie" }, 
+        { src: "../img/carrusel2.webp", titulo: "../img/cartitulo2.webp", descripcion: "\n18+   2 temporadas \nSerie posapocalíptica inspirada en el aclamado videojuego que sigue \nla brutal y desgarradora travesía de un hombre cínico y una joven de 14 años.", textbtn:"Ir a la serie" }, 
+        { src: "../img/carrusel3.webp", titulo: "../img/cartitulo3.webp", descripcion: "\n7+   2024 \nLa aventura épica de un robot que, tras naufragar en una \nisla deshabitada, aprende a sobrevivir relacionándose con la fauna local.", textbtn:"Ir a la película" }, 
+        { src: "../img/carrusel4.webp", titulo: "../img/cartitulo4.webp", descripcion: "\n18+   2011 \nEn la aterradora quinta entrega de la saga, la muerte \nvuelve para cobrar la vida de los supervivientes del colapso de un puente.", textbtn:"Ir a la película" }, 
+        { src: "../img/carrusel5.webp", titulo: "../img/cartitulo5.webp", descripcion: "\n16+   2024 \nUn equipo de buceadores se enfrenta a un tiburón toro en las \nprofundidades y a un grupo de despiadados criminales en la superficie.", textbtn:"Ir a la película" }, 
+        { src: "../img/carrusel6.webp", titulo: "../img/cartitulo6.webp", descripcion: "\n13+   2025 \nEn medio de una crisis internacional, cuatro magnates de la \ntecnología buscan la forma de usar el caos para su propio beneficio.", textbtn:"Ir a la película" } ];
 
     const siguiente = () => {
         if (indice < peliculas.length - 1) {
@@ -20,8 +26,8 @@ const Carrusel = () => {
         }
     };
 
-    return (//OTRA VEZ
-        <div id="carouselExampleDark" className="carousel slide" data-bs-ride="carousel">
+    return (//OTRA VEZ {`carousel slide ${styles.carousel}`}
+        <div id="carouselExampleDark" className={`carousel slide ${styles.carousel}`} data-bs-ride="carousel">
             {/*PROGRESOS*/}
             <div className="carousel-indicators">
                 {peliculas.map((_, i) => (
@@ -40,13 +46,13 @@ const Carrusel = () => {
                 {peliculas.map((pelicula, i) => (
                     <div key={i} className={`carousel-item ${i === indice ? "active" : ""}`}>
                         <Poster src={pelicula.src} alt={pelicula.titulo} />
-                        <Descripcion src={pelicula.titulo} descripcion={pelicula.descripcion} />
+                        <Descripcion src={pelicula.titulo} descripcion={pelicula.descripcion} textbtn={pelicula.textbtn} />                        
                     </div>
                 ))}
             </div>
 
-            <Boton clase="carousel-control-prev" dataTarget="#carouselExampleDark" dataSlide="prev" />
-            <Boton clase="carousel-control-next" dataTarget="#carouselExampleDark" dataSlide="next" />
+            <Boton clase={`carousel-control-prev ${styles.carouselcontrolpre}`} dataTarget="#carouselExampleDark" dataSlide="prev" index={indice} onSelect={anterior} />
+            <Boton clase={`carousel-control-next ${styles.carouselcontrolsig}`} dataTarget="#carouselExampleDark" dataSlide="next" index={indice} onSelect={siguiente} />
 
         </div>
     );
